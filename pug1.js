b294dc0840ce66350823cb18e7e8bcef08ece581
harshil2024-gh <harshil.9011@gmail.com>
2025-05-01 10:38:31 +0530

    Add files via upload

---
+++ pug1.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+app.set("view engine","pug")
+app.get("/",(req,res)=>
+    {
+        res.render(__dirname+"/pug1")
+    })
+app.listen(5050)
